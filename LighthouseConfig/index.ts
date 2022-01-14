46287e473e6db1aee30405bf39b348a2817446bf
Christopher Berge Hove <chbh@equinor.com>
2022-01-14 10:41:57 +0100

    validation on environmentId

--- LighthouseConfig/index.ts
+++ LighthouseConfig/index.ts
@@ -12,34 +12,15 @@ const client = new AppConfigurationClient(
   credential
 );
 
-async function run(context: Context, request: HttpRequest) {
-  const environmentId = decrypt(
-    'environmentId',
-    request.query.environmentId || ''
-  );
-
-  const response = await client.listConfigurationSettings({
-    labelFilter: environmentId,
-  });
-
-  const settings: ConfigurationSetting<string>[] = [];
-  for await (const setting of response) {
-    settings.push(setting);
+function isValid(id: string): boolean {
+  switch (id) {
+    case 'prod':
+    case 'dev':
+    case 'qa':
+      return true;
+    default:
+      return false;
   }
-
-  const settingsBody = settings.reduce((acc, item) => {
-    acc[item.key.toString()] = JSON.parse(item.value);
-    return acc;
-  }, {} as { [key: string]: string });
-
-  context.res = {
-    // status: 200, /* Defaults to 200 */
-    body: {
-      host: request.headers.host,
-      url: request.url,
-      ...settingsBody,
-    },
-  };
 }
 
 const httpTrigger: AzureFunction = async function (
@@ -53,10 +34,59 @@ const httpTrigger: AzureFunction = async function (
       status: 404,
       body: {
         error,
-        isDone: true,
       },
     };
   }
 };
 
 export default httpTrigger;
+
+async function run(context: Context, request: HttpRequest) {
+  if (!request.query.environmentId) {
+    context.res = {
+      status: 400,
+      body: {
+        error: 'environmentId query-parameter is not provided.',
+      },
+    };
+    return;
+  }
+
+  const environmentId = decrypt('environmentId', request.query.environmentId);
+
+  if (isValid(environmentId)) {
+    const settingsBody = await getSettings(environmentId);
+
+    context.res = {
+      // status: 200, /* Defaults to 200 */
+      body: {
+        host: request.headers.host,
+        url: request.url,
+        ...settingsBody,
+      },
+    };
+  } else {
+    context.res = {
+      status: 400,
+      body: {
+        error: 'environmentId is not valid.',
+      },
+    };
+  }
+}
+
+async function getSettings(environmentId: string) {
+  const response = await client.listConfigurationSettings({
+    labelFilter: environmentId,
+  });
+
+  const settings: ConfigurationSetting<string>[] = [];
+  for await (const setting of response) {
+    settings.push(setting);
+  }
+
+  return settings.reduce((acc, item) => {
+    acc[item.key.toString()] = JSON.parse(item.value);
+    return acc;
+  }, {} as { [key: string]: string });
+}
