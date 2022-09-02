ad25bfc50ce65728c1b846b4b877d91f24abef68
Joachim <joachim290@outlook.com>
2022-09-02 10:48:24 +0200

    add delete option for service msg

--- commonResources/appConfig/serviceMessage.ts
+++ commonResources/appConfig/serviceMessage.ts
@@ -1,40 +1,64 @@
 import { Context, HttpRequest } from "@azure/functions";
 import { SYSTEM_KEY } from "../constants/constants";
-import { systemMessageOutputValidation, systemMessageValidation as serviceMessageValidation } from "../utils/validation";
+import {
+  systemMessageOutputValidation,
+  systemMessageValidation as serviceMessageValidation,
+} from "../utils/validation";
 import { appConfigurationClient } from "./appConfigurationClient";
 
-export async function getServiceMessage(context: Context, request: HttpRequest) {
-    try{
-        const result = await appConfigurationClient.getConfigurationSetting({key: SYSTEM_KEY})
-        const systemMessage =  systemMessageOutputValidation(result.value)
-        
-        context.res = {
-            // status: result.statusCode,
-            body: {...systemMessage},
-    
-        };
-    } catch (error) {
-        context.res = error
-    }
+export async function getServiceMessage(
+  context: Context,
+  request: HttpRequest
+) {
+  try {
+    const result = await appConfigurationClient.getConfigurationSetting({
+      key: SYSTEM_KEY,
+    });
+    const systemMessage = systemMessageOutputValidation(result.value);
+
+    context.res = {
+      // status: result.statusCode,
+      body: { ...systemMessage },
+    };
+  } catch (error) {
+    context.res = error;
+  }
 }
 
 export async function postServiceMessage(context: Context, req: HttpRequest) {
-    try{
-        const value = serviceMessageValidation(req.body)
-        if (typeof value !== "string") throw value;
-            const result = await appConfigurationClient.setConfigurationSetting( {key: SYSTEM_KEY,  value })
-            
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: { status: "done", result },
-     
-          
-        };
-    } catch (error) {
-        context.res = {
-            ...error,
-     
-        };
-    }
+  try {
+    const value = serviceMessageValidation(req.body);
+    if (typeof value !== "string") throw value;
+    const result = await appConfigurationClient.setConfigurationSetting({
+      key: SYSTEM_KEY,
+      value,
+    });
+
+    context.res = {
+      // status: 200, /* Defaults to 200 */
+      body: { status: "done", result },
+    };
+  } catch (error) {
+    context.res = {
+      ...error,
+    };
+  }
+}
+export async function deleteServiceMessage(context: Context) {
+  try {
+    const result = await appConfigurationClient.setConfigurationSetting({
+      key: SYSTEM_KEY,
+      value: "",
+    });
+
+    context.res = {
+      // status: 200, /* Defaults to 200 */
+      body: { status: "done", result },
+    };
+  } catch (error) {
+    context.res = {
+      ...error,
+    };
+  }
 }
 
