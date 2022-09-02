39339ad7073edc88ef7192d17143465d383e9048
Joachim <joachim290@outlook.com>
2022-09-02 07:46:34 +0200

    fix: use getTime for date validation

--- commonResources/utils/validation.ts
+++ commonResources/utils/validation.ts
@@ -1,78 +1,79 @@
-import { SystemMessage } from "../types/systemMessage"
+import { SystemMessage } from "../types/systemMessage";
 
-export function systemMessageValidation(systemMessage?: SystemMessage): {  status: number , body: { message: string,  missingKeys?: string[]}} | string {
-    const keys = ["message", "type", "fromDate", "toDate"]
-    if (!systemMessage) {
-        return {  
-            status: 422,  
-            body: {message: "No valid Body provided",  
-            missingKeys: keys}
-        }
+export function systemMessageValidation(
+  systemMessage?: SystemMessage
+):
+  | { status: number; body: { message: string; missingKeys?: string[] } }
+  | string {
+  const keys = ["message", "type", "fromDate", "toDate"];
+  if (!systemMessage) {
+    return {
+      status: 422,
+      body: { message: "No valid Body provided", missingKeys: keys },
+    };
+  }
+  const missingKeys: string[] = [];
+  keys.map((key) => {
+    if (!Object.keys(systemMessage).includes(key)) {
+      missingKeys.push(key);
     }
-    const missingKeys: string[] = []
-    keys.map(key => {
-        if(!Object.keys(systemMessage).includes(key)) {
-            missingKeys.push(key) 
-        }
-    })
+  });
 
-    if (missingKeys.length > 0) {
-        return {  status: 422 , body: { message: "Key missing in body",  missingKeys} }
-    }
+  if (missingKeys.length > 0) {
+    return {
+      status: 422,
+      body: { message: "Key missing in body", missingKeys },
+    };
+  }
 
-    if (isValidDate(systemMessage.fromDate)){
-        return {  status: 422 , body: { message: "invalidDate fromDate" } }
-    }
+  if (isValidDate(systemMessage.fromDate)) {
+    return { status: 422, body: { message: "invalidDate fromDate" } };
+  }
 
-    if (isValidDate(systemMessage.toDate)){
-        return {  status: 422 , body: { message: "invalidDate toDate" } }
-    }
-    systemMessage.id = generateQuickGuid();
-    return JSON.stringify(systemMessage);
+  if (isValidDate(systemMessage.toDate)) {
+    return { status: 422, body: { message: "invalidDate toDate" } };
+  }
+  systemMessage.id = generateQuickGuid();
+  return JSON.stringify(systemMessage);
 }
-
 
 function generateQuickGuid() {
-    return Math.random().toString(36).substring(2, 15) +
-        Math.random().toString(36).substring(2, 15);
+  return (
+    Math.random().toString(36).substring(2, 15) +
+    Math.random().toString(36).substring(2, 15)
+  );
 }
 
-
-export function systemMessageOutputValidation(data?: string): SystemMessage | undefined {
-
-
-    try{
-        const systemMessage = JSON.parse(data)
-        const currentDate = new Date().getDate()
-        const toDate = new Date(systemMessage?.toDate).getDate();
-        const fromDate = new Date(systemMessage?.toDate).getDate();
-        if (currentDate <= fromDate && toDate >= currentDate) {
-            return systemMessage;
-        }
-    
-        return;
-    } catch {
-        return;
+export function systemMessageOutputValidation(
+  data?: string
+): SystemMessage | undefined {
+  try {
+    const systemMessage = JSON.parse(data);
+    const currentDate = new Date().getTime();
+    const toDate = new Date(systemMessage?.toDate).getTime();
+    const fromDate = new Date(systemMessage?.fromDate).getDate();
+    if (currentDate <= fromDate && toDate >= currentDate) {
+      return systemMessage;
     }
 
-
+    return;
+  } catch {
+    return;
+  }
 }
-
-
 
 function isValidDate(date: string): boolean {
-    return date === "" || (Date.parse(date).toString() === "NaN")
+  return date === "" || Date.parse(date).toString() === "NaN";
 }
 
-
 export function isValidEnvironment(id: string): boolean {
-    switch (id) {
-      case 'prod':
-      case 'dev':
-      case 'test':
-        return true;
-      default:
-        return false;
-    }
+  switch (id) {
+    case "prod":
+    case "dev":
+    case "test":
+      return true;
+    default:
+      return false;
   }
+}
 
